d24991b9ee50bd6fe40c4ec6e26fbc6440de17e9
nguyenduyhieu202 <nguyenduyhieu202@gmail.com>
2025-10-31 21:39:25 +0700

    addUnitTestAndMdFile

---
+++ Uracle.Tests/Commands/UsersCommands/UserLoginValidatorTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Uracle.Application.Commands.UsersCommand;
+using Uracle.Application.DTOs;
+namespace Uracle.Tests.Commands.UsersCommands
+{
+    [TestFixture]
+    public class UserLoginCommandValidatorTests
+    {
+        [Test]
+        public void Validate_ShouldFail_WhenUsernameIsEmpty()
+        {
+            var validator = new UserLoginCommandValidator();
+            var cmd = new UserLoginCommand(new UserLoginDTO("", "abcdef"));
+            var result = validator.Validate(cmd);
+            result.IsValid.Should().BeFalse();
+        }
+
+        [Test]
+        public void Validate_ShouldFail_WhenPasswordShorterThan6Chars()
+        {
+            var validator = new UserLoginCommandValidator();
+            var cmd = new UserLoginCommand(new UserLoginDTO("john", "abc"));
+            var result = validator.Validate(cmd);
+            result.IsValid.Should().BeFalse();
+        }
+
+        [Test]
+        public void Validate_ShouldPass_WhenUsernameAndPasswordValid()
+        {
+            var validator = new UserLoginCommandValidator();
+            var cmd = new UserLoginCommand(new UserLoginDTO("john", "abcdef"));
+            var result = validator.Validate(cmd);
+            result.IsValid.Should().BeTrue();
+        }
+    }
+}
